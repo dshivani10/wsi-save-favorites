0750c24a717f6e82dc2384df9f2dcf392438fed0
sdevasani <sdevasani@nisum.com>
2022-11-01 12:45:20 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <div>Save favorites MFE</div>
+  </div>
+</template>
+<script>
+import Header from "remote/Header";
+
+export default{
+  components:{
+    Header,
+  }
+}
+</script>
