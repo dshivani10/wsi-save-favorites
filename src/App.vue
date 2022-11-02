512c1c8f90133018006c69889ce3b22e3a5b6c44
sdevasani <sdevasani@nisum.com>
2022-11-02 12:28:08 +0530

    imported footer from global mfe

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div class="mt-10 text-3xl mx-auto max-w-6xl">
     <Header/>
     <div>Save favorites MFE</div>
+    <Footer/>
   </div>
 </template>
 <script>
 import Header from "remote/Header";
+import Footer from "remote/Footer";
 
 export default{
   components:{
     Header,
+    Footer,
   }
 }
 </script>
